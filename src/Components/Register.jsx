838044f90fb54bfc98e32cd6a6ddcfb590143017
maureen <maureen.murimi@moringaschool.com>
2024-06-24 13:01:52 +0300

    Utilized formData for form state management and handleChange for user input handling.

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -64,3 +64,75 @@ function Register() {
         
         
     }
+    return (
+        <div className='register'>
+            <div className='form-container'>
+                <h1 className='heading'>Dedanite Online Store</h1>
+                <form className='form' onSubmit={handleSubmit}>
+                    <h2>Create an account</h2>
+                    <input
+                        name='firstname'
+                        type='text'
+                        placeholder='First Name'
+                        value={formData.firstname}
+                        onChange={handleChange}
+                    />
+
+                    <input
+                        name='lastname'
+                        type='text'
+                        placeholder='Last Name'
+                        value={formData.lastname}
+                        onChange={handleChange}
+                    />
+
+                    <input
+                        name='phone'
+                        type='text'
+                        placeholder='Phone number'
+                        value={formData.phone}
+                        onChange={handleChange}
+                    />
+
+                    <input
+                        name='email'
+                        type='email'
+                        placeholder='Email Address'
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+
+                    <input
+                        name='password'
+                        type='password'
+                        placeholder='Password'
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+
+                    <input
+                        name='confirmPassword'
+                        type='password'
+                        placeholder='Confirm Password'
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                    />
+
+                    {error ? (<div className='error'>{error}</div>):("")}
+                    {notification ? (<div className='success'>{notification}</div>):("")}
+
+                    <button type='submit' className='button'>
+                        Register
+                    </button>
+                    <p>Already have an account ?<a href='/login'>Login</a> </p>
+                </form>
+                <div className='image'>
+                    <img src='https://i.im.ge/2024/02/08/cLhGuW.logo-color.png' />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Register
