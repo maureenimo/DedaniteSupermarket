ad70455cab759f62f7d6589ac3b62ef3d7f80de0
maureen <maureen.murimi@moringaschool.com>
2024-06-24 12:38:18 +0300

    Enhanced registration flow with error handling and success notification

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -26,3 +26,41 @@ function Register() {
 
     const handleSubmit = (event) => {
         event.preventDefault()
+        
+        if (formData.password == formData.confirmPassword){
+            fetch('https://dedanite-online.onrender.com/register',{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(formData)
+            })
+            .then((response) => {
+                if (response.ok){
+                    response.json()
+                    
+                    setNotification('Successfully Registered!')
+                    setTimeout(() => {
+                        navigate('/login')
+                    }, 3000);
+                    
+                }else{
+                    return response.json().then((errorData) => {
+                        throw new Error (`${errorData.error}`)
+                    })
+                }
+
+            })
+            .catch((error) => {
+                setError(error.message)
+                setTimeout(() => {
+                    setError(null)
+                }, 1000);
+            })
+
+        } else{
+            setError('password is not same')
+        }
+        
+        
+    }
