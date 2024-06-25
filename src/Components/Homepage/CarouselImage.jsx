0ec0e2ccabf82247a17f2384e71787ff0f40b817
maureen <maureen.murimi@moringaschool.com>
2024-06-25 12:23:06 +0300

    Added the Banner Image

---
+++ src/Components/Homepage/CarouselImage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExampleCarouselImage = () => {
+  return (
+    <div>
+      <img src="banner.png" alt="First slide" />
+    </div>
+  );
+};
+
+export default ExampleCarouselImage;
